90576b9836e84672ab5c166ceb24a71fd5a4b9e0
Matheus Sanchez <matheus2.sanchez@usp.br>
2020-08-31 23:26:33 -0300

    Importing Files Working

--- src/pages/Import/index.tsx
+++ src/pages/Import/index.tsx
@@ -23,19 +23,30 @@ const Import: React.FC = () => {
   const history = useHistory();
 
   async function handleUpload(): Promise<void> {
-    // const data = new FormData();
+    const data = new FormData();
 
+    if (!uploadedFiles.length) return;
+
+    const file = uploadedFiles[0];
+    data.append('file', file.file, file.name);
     // TODO
 
     try {
-      // await api.post('/transactions/import', data);
+      await api.post('/transactions/import', data);
+      history.push('/');
     } catch (err) {
-      // console.log(err.response.error);
+      console.log(err.response.error);
     }
   }
 
   function submitFile(files: File[]): void {
-    // TODO
+    const uploudFiles = files.map(file => ({
+      file,
+      name: file.name,
+      readableSize: filesize(file.size),
+    }));
+    console.log(uploudFiles);
+    setUploadedFiles(uploudFiles);
   }
 
   return (
